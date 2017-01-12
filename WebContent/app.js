0b0a01a991e569b853316f400a542c348b05947f
K.Saiteja <ksaiteja844@gmail.com>
2017-01-12 12:52:15 +0530

    Got so many errors by adding CDN full version

--- WebContent/app.js
+++ WebContent/app.js
@@ -5,11 +5,13 @@ app.config(function($routeProvider)
 	$routeProvider
 		
 .when('/',{
-	templateUrl : 'g_header/header.html'
+	templateUrl : 'g_home/home.html'
 	})
-	.when("/home",{
+	.when('/home',{
 		templateUrl :'g_home/home.html'
-	});		
-	
+	})
+	.otherwise({
+		redirectTo: "/"
+	})
 	
 		});
